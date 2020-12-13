78f3121e1750c7cbc9ce7ef43c705afdc126afcd
supriyakundu1999 <supriya.kundu.1999@gmail.com>
2020-12-13 17:52:41 +0530

    Bug fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 import { HomeComponent } from './home/home.component';
-
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: HomeComponent },
-    { path: 'auth', loadChildren: './auth/auth.module#AuthModule' }
+    { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
 ];
 
 @NgModule({
