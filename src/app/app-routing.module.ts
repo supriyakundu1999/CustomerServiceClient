069c64b846fda440c9fc96d76dddd00965f648b6
Supriya Kundu <supriya.kundu.1999@gmail.com>
2020-12-13 13:47:41 +0530

    Home added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: HomeComponent },
     { path: 'auth', loadChildren: './auth/auth.module#AuthModule' }
 ];
 
