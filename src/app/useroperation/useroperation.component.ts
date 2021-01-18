5a76f54f243e40aee6e714e6c36edd804eec6d79
Supriya Kundu <supriya.kundu.1999@gmail.com>
2021-01-18 12:29:30 +0530

    User Info form designed

--- src/app/useroperation/useroperation.component.ts
+++ src/app/useroperation/useroperation.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-useroperation',
-  templateUrl: './useroperation.component.html',
-  styleUrls: ['./useroperation.component.css']
+    selector: 'app-useroperation',
+    templateUrl: './useroperation.component.html',
+    styleUrls: ['./useroperation.component.css']
 })
 export class UseroperationComponent implements OnInit {
 
-  constructor() { }
+    userInfoForm: FormGroup
+    csrfKey: String = ''
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private formBuilder: FormBuilder,
+    ) { }
+
+    ngOnInit(): void {
+
+        this.userInfoForm = this.formBuilder.group({
+            csrfToken: [this.csrfKey, Validators.required],
+            stuName: ['', Validators.required],
+            class: ['', Validators.required],
+            stream: ['', Validators.required]
+        })
+    }
+
+    submitUserInfo(): void {
+        console.log("Submit User Info fn called...")
+        console.log(this.userInfoForm.value)
+    }
 
 }
--- src/app/useroperation/useroperation.module.ts
+++ src/app/useroperation/useroperation.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { UseroperationRoutingModule } from './useroperation-routing.module';
 import { UseroperationComponent } from '../useroperation/useroperation.component';
 
 
 @NgModule({
-  declarations: [UseroperationComponent],
-  imports: [
-    CommonModule,
-    UseroperationRoutingModule
-  ]
+    declarations: [UseroperationComponent],
+    imports: [
+        CommonModule,
+        UseroperationRoutingModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ]
 })
 export class UseroperationModule { }
